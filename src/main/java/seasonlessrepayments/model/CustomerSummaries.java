3df3e348d399344d477053c32386d0d2d5b27884
oceenachi <phillipfrancisca@gmail.com>
2020-03-24 20:19:54 +0100

    Model entity for customer summaries

---
+++ src/main/java/seasonlessrepayments/model/CustomerSummaries.java
@@ -0,0 +1,61 @@
+package seasonlessrepayments.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class CustomerSummaries {
+
+    @Id
+    private Long customerID;
+
+    @NotNull
+    private Long seasonID;
+
+    @NotNull
+    private Long totalRepaid;
+
+    @NotNull
+    private Long totalCredit;
+
+    public CustomerSummaries() {
+    }
+
+    public Long getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(Long customerID) {
+        this.customerID = customerID;
+    }
+
+    public Long getSeasonID() {
+        return seasonID;
+    }
+
+    public void setSeasonID(Long seasonID) {
+        this.seasonID = seasonID;
+    }
+
+    public Long getTotalRepaid() {
+        return totalRepaid;
+    }
+
+    public void setTotalRepaid(Long totalRepaid) {
+        this.totalRepaid = totalRepaid;
+    }
+
+    public Long getTotalCredit() {
+        return totalCredit;
+    }
+
+    public void setTotalCredit(Long totalCredit) {
+        this.totalCredit = totalCredit;
+    }
+
+
+
+
+}
