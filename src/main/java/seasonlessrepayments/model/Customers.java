ff17282c5861e0ceda3da7f8b1de9fb28d7081b3
oceenachi <phillipfrancisca@gmail.com>
2020-03-24 20:17:42 +0100

    Model customers entity

---
+++ src/main/java/seasonlessrepayments/model/Customers.java
@@ -0,0 +1,40 @@
+package seasonlessrepayments.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class Customers {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String customerName;
+
+    public Customers() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+
+}
