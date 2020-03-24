b226a993b8669ddc5b9fc7ffae36db5b76598b69
oceenachi <phillipfrancisca@gmail.com>
2020-03-24 20:14:22 +0100

    Add dependencies to pom.xml file

---
+++ src/main/java/seasonlessrepayments/SeasonlessRepaymentsApplication.java
@@ -0,0 +1,13 @@
+package seasonlessrepayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeasonlessRepaymentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeasonlessRepaymentsApplication.class, args);
+    }
+
+}
